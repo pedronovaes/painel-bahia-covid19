82cb9435ea88a938edd8e36bbec278357faa59c6
pedronovaes <pedronovaesmelo@gmail.com>
2020-07-01 01:57:33 -0300

    Adding index page

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,14 @@
 from app import app
+from app.dashboard import appdash
+from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'hello from flask'
+    return render_template('index.html')
+
+
+# @app.route('/dashboard')
+# def dashboard():
+#     return appdash.index()
