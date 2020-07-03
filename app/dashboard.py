84f4b54faa87ada76b3c9f8546c6a3aed7729725
pedronovaes <pedronovaesmelo@gmail.com>
2020-07-02 23:50:41 -0300

    Creating initial structure to display cities stats

--- app/dashboard.py
+++ app/dashboard.py
@@ -1,5 +1,6 @@
 from app import app
 from dash import Dash
+import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
@@ -11,6 +12,7 @@ appdash = Dash(
     external_stylesheets=[dbc.themes.FLATLY]
 )
 
+# Header
 info_bar = html.Div(
     children=html.Ul(
         className='navbar-nav',
@@ -76,5 +78,99 @@ def toggle_navbar_collapse(n, is_open):
     return is_open
 
 
+# Body
+body_cities_stats = dbc.Container(
+    children=[
+        dbc.Row([
+            dbc.Col(
+                dbc.Card(
+                    dbc.CardBody([
+                        html.H5('Selecione uma cidade'),
+                        dcc.Dropdown(
+                            id='cities-dropdown',
+                            options=[
+                                {'label': 'Bahia', 'value': 'Bahia'},
+                                {'label': 'Salvador', 'value': 'Salvador'},
+                                {'label': 'Irecê', 'value': 'Irecê'},
+                            ],
+                            value='Bahia',
+                            clearable=False,
+                            searchable=True,
+                            style={
+                                'color': '#1F242D',
+                                'marginTop': '15px'
+                            },
+                        )
+                    ]),
+                    color='primary',
+                    inverse=True,
+                    style={'height': '14vh', 'textAlign': 'center'},
+                ),
+                lg=3, xs=12,
+                style={'marginBottom': '10px'}
+            ),
+            dbc.Col(
+                dbc.Card(
+                    children=[
+                        dbc.CardBody(
+                            html.H3(
+                                '1.100.245',
+                                style={'color': 'yellow', 'fontWeight': 'bold', 'fontSize': '250%'}
+                            ),
+                        ),
+                        dbc.CardFooter('Confirmados'),
+                    ],
+                    color='primary',
+                    inverse=True,
+                    style={'height': '14vh', 'textAlign': 'center'},
+                ),
+                lg=3, xs=12,
+                style={'marginBottom': '10px'}
+            ),
+            dbc.Col(
+                dbc.Card(
+                    children=[
+                        dbc.CardBody(
+                            html.H3(
+                                '1.245',
+                                style={'color': 'red', 'fontWeight': 'bold', 'fontSize': '250%'}
+                            ),
+                        ),
+                        dbc.CardFooter('Mortos'),
+                    ],
+                    color='primary',
+                    inverse=True,
+                    style={'height': '14vh', 'textAlign': 'center'},
+                ),
+                lg=3, xs=12,
+                style={'marginBottom': '10px'}
+            ),
+            dbc.Col(
+                dbc.Card(
+                    children=[
+                        dbc.CardBody(
+                            html.H3(
+                                '1.245',
+                                style={'color': 'white', 'fontWeight': 'bold', 'fontSize': '250%'}
+                            ),
+                        ),
+                        dbc.CardFooter('Taxa de morte'),
+                    ],
+                    color='primary',
+                    inverse=True,
+                    style={'height': '14vh', 'textAlign': 'center'},
+                ),
+                lg=3, xs=12,
+                style={'marginBottom': '10px'}
+            ),
+        ])
+    ],
+    fluid=True,
+    style={'marginTop': '20px'}
+)
+
+
 appdash.title = 'Painel Bahia covid-19 | Dashboard'
-appdash.layout = html.Div(navbar)
+appdash.layout = html.Div([
+    navbar, body_cities_stats
+])
